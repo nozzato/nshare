aadf4086e62d516048f493b2c003ae7915d93bd0
Noah Torrance <noahtorrance27@gmail.com>
2022-05-13 17:17:16 +0100

    Allow uploads from $_POST

--- upload.php
+++ upload.php
@@ -3,15 +3,25 @@ session_start();
 include_once('functions.php');
 
 if(isset($_POST['upload_btn'])) {
-    if(isset($_FILES['file'])){
-        if($_SESSION['page'] == 'public') {
-            $file_path = 'files/public/';
-        } else if($_SESSION['page'] == 'private') {
-            $file_path = 'files/' . $_SESSION['username'] . '/';
-        }
+    if($_SESSION['page'] == 'public') {
+        $file_path = 'files/public/';
+    } else if($_SESSION['page'] == 'private') {
+        $file_path = 'files/' . $_SESSION['username'] . '/';
+    }
+    if(isset($_FILES['file'])) {
         $file_name = $_FILES['file']['name'];
 
         if(move_uploaded_file($_FILES['file']['tmp_name'], $file_path . $file_name)) {
+            chmod( $file_path . $file_name, 0775);
+
+            $_SESSION['msg'] = "File uploaded";
+        } else {
+            $_SESSION['msg'] = "Error: Upload failed";
+        }
+    } else if(isset($_POST['file'])) {
+        $file_name = $_POST['file']['name'];
+
+        if(move_uploaded_file($_POST['file']['tmp_name'], $file_path . $file_name)) {
             chmod( $file_path . $file_name, 0775);
 
             $_SESSION['msg'] = "File uploaded";
