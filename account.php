c214b88ca29ebefb951f4cb36bbb4606c9371576
Noah Torrance <noahtorrance27@gmail.com>
2022-05-17 10:51:46 +0100

    Remove redundant code

--- account.php
+++ account.php
@@ -41,30 +41,22 @@ $_SESSION['page'] = 'account';
                 <i class="fa fa-folder-open"></i> Files <i class="fa fa-caret-down"></i>
             </button>
             <div class="w3-dropdown-content w3-bar-block nz-black nz-round-bottom w3-card-2 w3-hide-small">
-            <?php if(isset($_SESSION['user_id'])) { ?>
                 <a class="w3-bar-item w3-button" href="public.php">
                     <i class="fa fa-globe"></i> Public
                 </a>
+            <?php if(isset($_SESSION['user_id'])) { ?>
                 <a class="w3-bar-item w3-button nz-round-bottom" href="private.php">
                     <i class="fa fa-lock"></i> Private
-                </a>
-            <?php } else { ?>
-                <a class="w3-bar-item w3-button nz-round-bottom" href="public.php">
-                    <i class="fa fa-globe"></i> Public
                 </a>
             <?php } ?>
             </div>
             <div class="w3-dropdown-content w3-bar-block nz-black nz-round-bottom-right w3-card-2 w3-hide-large w3-hide-medium">
-            <?php if(isset($_SESSION['user_id'])) { ?>
                 <a class="w3-bar-item w3-button" href="public.php">
                     <i class="fa fa-globe"></i> Public
                 </a>
+            <?php if(isset($_SESSION['user_id'])) { ?>
                 <a class="w3-bar-item w3-button nz-round-bottom-right" href="private.php">
                     <i class="fa fa-lock"></i> Private
-                </a>
-            <?php } else { ?>
-                <a class="w3-bar-item w3-button nz-round-bottom-right" href="public.php">
-                    <i class="fa fa-globe"></i> Public
                 </a>
             <?php } ?>
             </div>
