16b46eacd549cb9adf5257cb8b28c74b69f73454
Noah Torrance <noahtorrance27@gmail.com>
2022-05-26 13:58:12 +0100

    Increase file limit

--- files/upload.php
+++ files/upload.php
@@ -9,7 +9,7 @@ if(isset($_FILES['upload_file']) || isset($_POST['upload_btn'])) {
     if(isset($_FILES['upload_file'])) {
 
         $file_count = count($_FILES['upload_file']['name']);
-        if($file_count > 20) {
+        if($file_count > 100) {
             $_SESSION['msg'] = 'Error: Cannot upload more than 20 files at a time';
             go_back();
         }
