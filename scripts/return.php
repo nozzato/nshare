5a3119650dc7a567fae08e038e35a66ef6a8b9cd
Noah Torrance <noahtorrance27@gmail.com>
2022-06-19 14:17:06 +0100

    Add back button

---
+++ scripts/return.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+if($_SESSION['page'] == 'home') {
+    header('location:/index.php');
+    exit;
+} else if ($_SESSION['page'] == 'signup') {
+    header('location:/user/signup.php');
+    exit;
+} else if($_SESSION['page'] == 'profile') {
+    header('location:/user/index.php');
+    exit;
+} else if($_SESSION['page'] == 'settings') {
+    header('location:/user/settings.php');
+    exit;
+} else if($_SESSION['page'] == 'files') {
+    header('location:/files/index.php');
+    exit;
+} else if($_SESSION['page'] == 'admin') {
+    header('location:/admin/index.php');
+    exit;
+} else {
+    header('location:/index.php');
+    exit;
+}
+?>
