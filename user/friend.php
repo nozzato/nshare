f45e3c816cdf569ca7aa638fd662b7e1f2dfbd81
Noah Torrance <noahtorrance27@gmail.com>
2022-07-13 13:53:56 +0100

    Update both users when adding friends

--- user/friend.php
+++ user/friend.php
@@ -20,12 +20,29 @@ if($_GET['friendid']) {
     $stmt-> execute();
     $friends = $stmt-> fetch(PDO::FETCH_ASSOC);
 
+    // add friend to friends array
     $friends = substr($friends['array'], 0, -1);
     $friends = $friends . ',' . $friend_id . ']';
 
     // update friend in database
     $stmt = $pdo-> prepare('UPDATE `friends` SET `friends` = ? WHERE `user_id` = ?;');
     $stmt-> execute([$friends, $_SESSION['user']]);
+
+    // select target friends array
+    $stmt = $pdo-> prepare('SELECT `friends` INTO @friends FROM `friends` WHERE `user_id` = ?;');
+    $stmt-> execute([$friend_id]);
+
+    $stmt = $pdo-> prepare('SELECT @friends "array" ;');
+    $stmt-> execute();
+    $friends = $stmt-> fetch(PDO::FETCH_ASSOC);
+
+    // add user target friends array
+    $friends = substr($friends['array'], 0, -1);
+    $friends = $friends . ',' . $_SESSION['user'] . ']';
+
+    // update friend in database
+    $stmt = $pdo-> prepare('UPDATE `friends` SET `friends` = ? WHERE `user_id` = ?;');
+    $stmt-> execute([$friends, $friend_id]);
 }
 
 go_back();
