06c13b302f2dec742981c9d30433c8dda23011bc
Noah Torrance <noahtorrance27@gmail.com>
2022-10-14 13:13:58 +0100

    Hide users page button when banned

--- user/users.php
+++ user/users.php
@@ -79,12 +79,13 @@ function openProfile(id) {
         <a class='w3-bar-item w3-button' href='/files/index'>
             <i class='fa fa-fw fa-folder-open'></i> Files
         </a>
+    <?php if(isset($_SESSION['ban_status']) && $_SESSION['ban_status'] == 0) { ?>
         <a class='w3-bar-item w3-button' href='/user/users'>
             <i class='fa fa-fw fa-user'></i> Users
         </a>
-    <?php } ?>
+    <?php } } ?>
 
-    <?php if(isset($_SESSION['rank']) && $_SESSION['rank'] == 'admin') { ?>
+    <?php if(isset($_SESSION['rank']) && $_SESSION['rank'] == 'admin' && isset($_SESSION['ban_status']) && $_SESSION['ban_status'] == 0) { ?>
         <a class='w3-bar-item w3-button' href='/admin/index'>
             <i class='fa fa-fw fa-server'></i> Admin
         </a>
