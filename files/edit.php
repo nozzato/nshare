e0abde54f52158cc333f2993b7b8901f1d4d4840
Noah Torrance <noahtorrance27@gmail.com>
2023-05-20 11:41:23 +0100

    Allow users to view public files without an account

--- files/edit.php
+++ files/edit.php
@@ -1,12 +1,6 @@
 <?php
 session_start();
 
-// if not logged in
-if(!isset($_SESSION['user'])) {
-    header('location:/index');
-    exit;
-}
-
 // include functions
 include_once($_SERVER['DOCUMENT_ROOT'] . '/scripts/scripts.php');
 
